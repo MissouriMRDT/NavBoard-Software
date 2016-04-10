c7f203ea4677b6f0ff507688cf330a758f864b1a
Judah Schad <jrs6w7@mst.edu>
2016-04-09 19:47:00 -0500

    Gps custom shield software validated  READY to FIELD  TEST

--- SensorBoard-Software.ino
+++ SensorBoard-Software.ino
@@ -17,41 +17,50 @@ const uint16_t GPS_SATELLITES_DATA_ID = 306;
 
 long gps_lat_lon = 0;
 
-HardwareSerial mySerial = Serial7;
-Adafruit_GPS GPS(&mySerial);
+Adafruit_GPS GPS(&Serial7);
 //SoftwareSerial mySerial(3, 2);
 
 void setup()  
 {   
   // connect at 115200 so we can read the GPS fast enough and echo without dropping chars
   //Serial.begin(115200);
-  //Serial.println("Adafruit GPS library basic test!");
   
   //connect to roveComm
+  Ethernet.enableActivityLed();
+  Ethernet.enableLinkLed();
   roveComm_Begin(192,168,1,133);
   //Serial.println("roveComm_Begin");
   
-  // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
+  //9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
   GPS.begin(9600);
   
   GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
   
-  // Set the update rate
+  //Set the update rate
   GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
 
-  // Request updates on antenna status, comment out to keep quiet
-  GPS.sendCommand(PGCMD_ANTENNA);
-
-  delay(1000);
+  //Request updates on antenna status, comment out to keep quiet
+  //GPS.sendCommand(PGCMD_ANTENNA);
 
 }//end 
 
 uint32_t timer = millis();
 
-void loop()                     // run over and over again
+void loop() 
 {
+  uint16_t data_id = 0;
+  size_t data_size = 0;
+  uint16_t data = 0;
+  roveComm_GetMsg(&data_id, &data_size, &data);
+  delay(300);
+  
+  //int16_t msg = 0;
+  //roveComm_SendMsg(301, sizeof(msg), &msg);
+  //delay(300);
+  
+  //Serial.print("Looping");
   delay(1);
-   
+  
   char c = GPS.read();
   
   delay(1);
@@ -77,15 +86,15 @@ void loop()                     // run over and over again
     timer = millis(); // reset the timer
    
     //debug
-    //GPS.fixquality = 200;
-    //GPS.latitude_fixed = 407098514;
-    //GPS.longitude_fixed = -740079168;
-    //GPS.speed = 123.456;
-    //GPS.angle = 789.012;
-    //GPS.altitude = 345.678;
-    //GPS.satellites = 251;
+    GPS.fix = true;
+    GPS.fixquality = 200;
+    GPS.latitude_fixed = 407098514;
+    GPS.longitude_fixed = -740079168;
+    GPS.speed = 123.456;
+    GPS.angle = 789.012;
+    GPS.altitude = 345.678;
+    GPS.satellites = 251;
    
-    //Serial.print("Fix: "); Serial.print((int)GPS.fix);
     if(!GPS.fix)
     {
       GPS.fixquality = 0;
@@ -95,16 +104,12 @@ void loop()                     // run over and over again
     roveComm_SendMsg(GPS_FIX_QUALITY_DATA_ID, sizeof(GPS.fixquality), &GPS.fixquality);
     
     if (GPS.fix) 
-    {
-      //Serial.print("Location: ");
-      //Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
-      //Serial.print(", "); 
-      //Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
-      
-      //VARIFY ADAFRUIT_GPS PULL #13
+    {  
+      //TODO: VERIFY ADAFRUIT_GPS PULL #13
       gps_lat_lon = GPS.latitude_fixed;
       gps_lat_lon = gps_lat_lon << 32;
       gps_lat_lon += GPS.longitude_fixed;
+      
       roveComm_SendMsg(GPS_LAT_LON_DATA_ID, sizeof(gps_lat_lon), &gps_lat_lon);
       
       //Serial.print("Speed (knots): "); Serial.println(GPS.speed);
