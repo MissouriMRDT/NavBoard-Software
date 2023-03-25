a59945fb94b0afdf1b99bd54436d5bb766ab0d17
Grant Brinker <gsbfy2@umsystem.edu>
2023-03-25 15:27:01 -0500

    Worked on GPS rovecomm + cleaning

--- NavBoard_Software.h
+++ NavBoard_Software.h
@@ -1,29 +1,8 @@
-#pragma once
 #ifndef NAVBOARD_SOFTWARE_H
 #define NAVBOARD_SOFTWARE_H
 
 #include "RoveComm.h"
 #include <TinyGPS.h>
-
-// Pin definitions
-#define GPS_SCL             A17
-#define GPS_SDA             A16
-#define GPS_TX              29
-#define GPS_RX              28
-#define GPS_SERIAL_BAUD     9600
-#define GPS_SERIAL          Serial7
-
-#define BHI_SDA 10
-#define BHI_SCL 11
-
-#define ICM_MOSI    36
-#define ICM_MISO    35
-#define ICM_CS      37
-#define ICM_SCLK    14
-
-#define I2C_LED     28
-#define UART_LED    27
-#define SPI_LED     26
 
 RoveCommEthernet RoveComm;
 rovecomm_packet packet;
@@ -34,8 +13,39 @@ IntervalTimer Telemetry;
 // declaring Ethernet connection
 EthernetServer TCPServer(RC_ROVECOMM_NAVBOARD_PORT);
 
-// Function declarations
+////// Pin definitions //////
+// Compass
+#define GPS_SCL             A17
+#define GPS_SDA             A16
+
+// GPS
+#define GPS_TX              29
+#define GPS_RX              28
+
+// BHI
+#define BHI_SDA 10
+#define BHI_SCL 11
+
+// ICM
+#define ICM_MOSI    36
+#define ICM_MISO    35
+#define ICM_CS      37
+#define ICM_SCLK    14
+
+// LEDs
+#define I2C_LED     28
+#define UART_LED    27
+#define SPI_LED     26
+
+////// Variable definitions //////
+// GPS
+#define GPS_SERIAL_BAUD     9600
+#define GPS_SERIAL          Serial7
+float roverLatLon[2];
+
+////// Function declarations //////
 void gpsDump();
+void getGPSData();
 void getCompassData();
 void getBHIData();
 void getICMData();
--- NavBoard_Software.ino
+++ NavBoard_Software.ino
@@ -22,95 +22,108 @@ void setup()
     delay(100);
     Serial.println("begin");
 
-    //RoveComm.begin(RC_NAVBOARD_FOURTHOCTET, &TCPServer, RC_ROVECOMM_NAVBOARD_MAC);
-    //delay(100);
+    RoveComm.begin(RC_NAVBOARD_FOURTHOCTET, &TCPServer, RC_ROVECOMM_NAVBOARD_MAC);
+    delay(100);
 
-    //Telemetry.begin(telemetry, 1500000);
+    Telemetry.begin(telemetry, 1500000);
 }
 
 
 
 void loop()
 {
-  bool newdata = false;
-  unsigned long start = millis();
+    bool newdata = false;
+    unsigned long start = millis();
 
-  // Every 5 seconds we print an update
-  while (millis() - start < 5000) 
-  {
-    if (GPS_SERIAL.available()) 
+    // Every 5 seconds we print an update
+    while (millis() - start < 5000) 
     {
-      char c = GPS_SERIAL.read();
-      //Serial.print(c);  // uncomment to see raw GPS data
-      if (gps.encode(c)) 
-      {
-        newdata = true;
-      // break;  // uncomment to print new data immediately!
-      }
+        if (GPS_SERIAL.available()) 
+        {
+            char c = GPS_SERIAL.read();
+            //Serial.print(c);  // uncomment to see raw GPS data
+            if (gps.encode(c)) 
+            {
+                newdata = true;
+                // break;  // uncomment to print new data immediately!
+            }
+        }
     }
-  }
-  
-  if (newdata)
-  {
-    Serial.println("Acquired Data");
-    Serial.println("-------------");
-    gpsDump();
-    Serial.println("-------------");
-    Serial.println();
-  }
-    //getGPSData();
+    
+    if (newdata)
+    {
+        Serial.println("Acquired Data");
+        Serial.println("-------------");
+        gpsDump();
+        Serial.println("-------------");
+        Serial.println();
+    }
+
+
+    getGPSData();
     //getCompassData();
     //getBHIData();
     //getICMData();
-    //telemetry();
+    telemetry();
 }
 
 
 
 void telemetry()
 {
-
+    RoveComm.write(RC_NAVBOARD_GPSLATLON_DATA_ID, RC_NAVBOARD_GPSLATLON_DATA_COUNT, roverLatLon);
 }
 
 
 
 void gpsDump()
 {
-  static long lat, lon;
-  static unsigned long age, date, time, chars;
-  static int year;
-  byte month, day, hour, minute, second, hundredths;
-  static unsigned short sentences, failed;
+    static long lat, lon;
+    static unsigned long age, date, time, chars;
+    static int year;
+    byte month, day, hour, minute, second, hundredths;
+    static unsigned short sentences, failed;
 
-  gps.get_position(&lat, &lon, &age);
-  Serial.print("Lat/Long(10^-5 deg): "); Serial.print(lat); Serial.print(", "); Serial.print(lon); 
-  Serial.print(" Fix age: "); Serial.print(age); Serial.println("ms.");
-  
-  // On Arduino, GPS characters may be lost during lengthy Serial.print()
-  // On Teensy, Serial prints to USB, which has large output buffering and
-  //   runs very fast, so it's not necessary to worry about missing 4800
-  //   baud GPS characters.
+    gps.get_position(&lat, &lon, &age);
+    Serial.print("Lat/Long(10^-5 deg): "); Serial.print(lat); Serial.print(", "); Serial.print(lon); 
+    Serial.print(" Fix age: "); Serial.print(age); Serial.println("ms.");
+    
+    // On Arduino, GPS characters may be lost during lengthy Serial.print()
+    // On Teensy, Serial prints to USB, which has large output buffering and
+    //   runs very fast, so it's not necessary to worry about missing 4800
+    //   baud GPS characters.
 
-  Serial.print(" Fix age: "); Serial.print(age); Serial.println("ms.");
+    Serial.print(" Fix age: "); Serial.print(age); Serial.println("ms.");
 
-  gps.get_datetime(&date, &time, &age);
-  Serial.print("Date(ddmmyy): "); Serial.print(date); Serial.print(" Time(hhmmsscc): ");
-  Serial.print(time);
-  Serial.print(" Fix age: "); Serial.print(age); Serial.println("ms.");
+    gps.get_datetime(&date, &time, &age);
+    Serial.print("Date(ddmmyy): "); Serial.print(date); Serial.print(" Time(hhmmsscc): ");
+    Serial.print(time);
+    Serial.print(" Fix age: "); Serial.print(age); Serial.println("ms.");
 
-  gps.crack_datetime(&year, &month, &day, &hour, &minute, &second, &hundredths, &age);
-  Serial.print("Date: "); Serial.print(static_cast<int>(month)); Serial.print("/"); 
-  Serial.print(static_cast<int>(day)); Serial.print("/"); Serial.print(year);
-  Serial.print("  Time: "); Serial.print(static_cast<int>(hour)); Serial.print(":"); 
-  Serial.print(static_cast<int>(minute)); Serial.print(":"); Serial.print(static_cast<int>(second));
-  Serial.print("."); Serial.print(static_cast<int>(hundredths));
-  Serial.print("  Fix age: ");  Serial.print(age); Serial.println("ms.");
+    gps.crack_datetime(&year, &month, &day, &hour, &minute, &second, &hundredths, &age);
+    Serial.print("Date: "); Serial.print(static_cast<int>(month)); Serial.print("/"); 
+    Serial.print(static_cast<int>(day)); Serial.print("/"); Serial.print(year);
+    Serial.print("  Time: "); Serial.print(static_cast<int>(hour)); Serial.print(":"); 
+    Serial.print(static_cast<int>(minute)); Serial.print(":"); Serial.print(static_cast<int>(second));
+    Serial.print("."); Serial.print(static_cast<int>(hundredths));
+    Serial.print("  Fix age: ");  Serial.print(age); Serial.println("ms.");
 
-  Serial.print("Alt(cm): "); Serial.print(gps.altitude()); Serial.print(" Course(10^-2 deg): ");
-  Serial.print(gps.course()); Serial.print(" Speed(10^-2 knots): "); Serial.println(gps.speed());
-  gps.stats(&chars, &sentences, &failed);
-  Serial.print("Stats: characters: "); Serial.print(chars); Serial.print(" sentences: ");
-  Serial.print(sentences); Serial.print(" failed checksum: "); Serial.println(failed);
+    Serial.print("Alt(cm): "); Serial.print(gps.altitude()); Serial.print(" Course(10^-2 deg): ");
+    Serial.print(gps.course()); Serial.print(" Speed(10^-2 knots): "); Serial.println(gps.speed());
+    gps.stats(&chars, &sentences, &failed);
+    Serial.print("Stats: characters: "); Serial.print(chars); Serial.print(" sentences: ");
+    Serial.print(sentences); Serial.print(" failed checksum: "); Serial.println(failed);
+}
+
+
+
+void getGPSData()
+{
+    static float lat,lon;
+    static unsigned long age;
+    gps.f_get_position(&lat, &lon, &age);
+    roverLatLon[0] = lat;
+    roverLatLon[1] = lon;
 }
 
 
